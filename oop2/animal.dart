7e0269fda20eb921b874830cd332aad939b5b23a
Tsabit Mumtaz <mumtazidnetwork@gmail.com>
2026-09-02 11:25:42 +0700

    Add Dart fundamental projects

--- oop2/animal.dart
+++ oop2/animal.dart
@@ -1,23 +1,24 @@
 class Animal {
-  String name;
+  //attribute
+  String? name;
   double weight = 0;
-  String color;
-
-  Animal(this.name, this.weight, this.color);
-
-  void sleep (){
-    print('$name is sleeping');
-  }
-
+  String? color;
+ 
+  //constructor
+  Animal(this.name, this.color, this.weight);
+ 
+  //methods
   void eat() {
     print('$name is eating');
-    weight += 0.2 ;
+    weight += 0.2;
   }
-
-  void poop () {
+ 
+  void sleep() {
+    print('$name is sleeping');
+  }
+ 
+  void poop() {
     print('$name is pooping');
-    weight -= 0.1 ;
+    weight -= 0.1;
   }
-
-  
 }
--- oop2/plants.dart
+++ oop2/plants.dart
@@ -1,20 +1,36 @@
 class Plants {
-  //atribut
+  //attribute/properties
   String? name;
-  double? height;
+  String? latinName;
   String? color;
-
-  Plants(this.name, this.height, this.color);
-
+ 
   // Constructor
   /**
-   * Mmebuat inisialisasi lebih mudah dari pada satu per-satu
+   * Membuat insialisasi lebih mudah dari pada satu/satu
    * bisa juga di sebut sebagai method khusus
    * karena berbeda dengan method biasa
-   * 1. nama harus sama dengan class
-   * 2. tidak memiliki return type
-   * 3. dipanggil otomatis ketika objek dibuat
-   * 4. digunakan untuk inisialisasi objek
+   * 1. nama harus sama dengan nama class
+   * 2. tidak memiliki kembalian (return type)
+   * 3. dipanggil "otomatis" ketika objek dibuat
+   * 4. digunakna untuk inisialisasi objek
    */
+ 
+  Plants(this.name, this.latinName, this.color);
+ 
+  // Plants(String name, String latinName, String color) {
+  //   this.name = name;
+  //   this.latinName = latinName;
+  //   this.color = color;
+  // }
+ 
+  // named constructor
+  // ClassName.nameControctur(){}
+  Plants.name(this.name);
+  Plants.latinName(this.latinName);
+  Plants.color(this.color);
+ 
+  // Plants.fromJson();
+  // Plants.formDatabases();
+  // Plants.fromApi();
+ 
 }
-
